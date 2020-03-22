0762c2b8ca2d75b97cea5ac012976d6e9dc9a85c
donpapitoskey <59004502+donpapitoskey@users.noreply.github.com>
2020-03-21 20:01:15 -0500

    First update
    First update of project for instrumentation. It contains the whole code for the global implementation

---
+++ Intrumentacion.X/mcc_generated_files/pin_manager.h
@@ -0,0 +1,185 @@
+/**
+  @Generated Pin Manager Header File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the Pin Manager file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  @Description
+    This header file provides APIs for driver for .
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.80.0
+        Device            :  PIC16F18446
+        Driver Version    :  2.11
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 2.10 and above
+        MPLAB 	          :  MPLAB X 5.30	
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#ifndef PIN_MANAGER_H
+#define PIN_MANAGER_H
+
+/**
+  Section: Included Files
+*/
+
+#include <xc.h>
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set RA1 procedures
+#define RA1_SetHigh()            do { LATAbits.LATA1 = 1; } while(0)
+#define RA1_SetLow()             do { LATAbits.LATA1 = 0; } while(0)
+#define RA1_Toggle()             do { LATAbits.LATA1 = ~LATAbits.LATA1; } while(0)
+#define RA1_GetValue()              PORTAbits.RA1
+#define RA1_SetDigitalInput()    do { TRISAbits.TRISA1 = 1; } while(0)
+#define RA1_SetDigitalOutput()   do { TRISAbits.TRISA1 = 0; } while(0)
+#define RA1_SetPullup()             do { WPUAbits.WPUA1 = 1; } while(0)
+#define RA1_ResetPullup()           do { WPUAbits.WPUA1 = 0; } while(0)
+#define RA1_SetAnalogMode()         do { ANSELAbits.ANSA1 = 1; } while(0)
+#define RA1_SetDigitalMode()        do { ANSELAbits.ANSA1 = 0; } while(0)
+
+// get/set RA2 procedures
+#define RA2_SetHigh()            do { LATAbits.LATA2 = 1; } while(0)
+#define RA2_SetLow()             do { LATAbits.LATA2 = 0; } while(0)
+#define RA2_Toggle()             do { LATAbits.LATA2 = ~LATAbits.LATA2; } while(0)
+#define RA2_GetValue()              PORTAbits.RA2
+#define RA2_SetDigitalInput()    do { TRISAbits.TRISA2 = 1; } while(0)
+#define RA2_SetDigitalOutput()   do { TRISAbits.TRISA2 = 0; } while(0)
+#define RA2_SetPullup()             do { WPUAbits.WPUA2 = 1; } while(0)
+#define RA2_ResetPullup()           do { WPUAbits.WPUA2 = 0; } while(0)
+#define RA2_SetAnalogMode()         do { ANSELAbits.ANSA2 = 1; } while(0)
+#define RA2_SetDigitalMode()        do { ANSELAbits.ANSA2 = 0; } while(0)
+
+// get/set RB5 procedures
+#define RB5_SetHigh()            do { LATBbits.LATB5 = 1; } while(0)
+#define RB5_SetLow()             do { LATBbits.LATB5 = 0; } while(0)
+#define RB5_Toggle()             do { LATBbits.LATB5 = ~LATBbits.LATB5; } while(0)
+#define RB5_GetValue()              PORTBbits.RB5
+#define RB5_SetDigitalInput()    do { TRISBbits.TRISB5 = 1; } while(0)
+#define RB5_SetDigitalOutput()   do { TRISBbits.TRISB5 = 0; } while(0)
+#define RB5_SetPullup()             do { WPUBbits.WPUB5 = 1; } while(0)
+#define RB5_ResetPullup()           do { WPUBbits.WPUB5 = 0; } while(0)
+#define RB5_SetAnalogMode()         do { ANSELBbits.ANSB5 = 1; } while(0)
+#define RB5_SetDigitalMode()        do { ANSELBbits.ANSB5 = 0; } while(0)
+
+// get/set RB7 procedures
+#define RB7_SetHigh()            do { LATBbits.LATB7 = 1; } while(0)
+#define RB7_SetLow()             do { LATBbits.LATB7 = 0; } while(0)
+#define RB7_Toggle()             do { LATBbits.LATB7 = ~LATBbits.LATB7; } while(0)
+#define RB7_GetValue()              PORTBbits.RB7
+#define RB7_SetDigitalInput()    do { TRISBbits.TRISB7 = 1; } while(0)
+#define RB7_SetDigitalOutput()   do { TRISBbits.TRISB7 = 0; } while(0)
+#define RB7_SetPullup()             do { WPUBbits.WPUB7 = 1; } while(0)
+#define RB7_ResetPullup()           do { WPUBbits.WPUB7 = 0; } while(0)
+#define RB7_SetAnalogMode()         do { ANSELBbits.ANSB7 = 1; } while(0)
+#define RB7_SetDigitalMode()        do { ANSELBbits.ANSB7 = 0; } while(0)
+
+// get/set V_signal aliases
+#define V_signal_TRIS                 TRISCbits.TRISC0
+#define V_signal_LAT                  LATCbits.LATC0
+#define V_signal_PORT                 PORTCbits.RC0
+#define V_signal_WPU                  WPUCbits.WPUC0
+#define V_signal_OD                   ODCONCbits.ODCC0
+#define V_signal_ANS                  ANSELCbits.ANSC0
+#define V_signal_SetHigh()            do { LATCbits.LATC0 = 1; } while(0)
+#define V_signal_SetLow()             do { LATCbits.LATC0 = 0; } while(0)
+#define V_signal_Toggle()             do { LATCbits.LATC0 = ~LATCbits.LATC0; } while(0)
+#define V_signal_GetValue()           PORTCbits.RC0
+#define V_signal_SetDigitalInput()    do { TRISCbits.TRISC0 = 1; } while(0)
+#define V_signal_SetDigitalOutput()   do { TRISCbits.TRISC0 = 0; } while(0)
+#define V_signal_SetPullup()          do { WPUCbits.WPUC0 = 1; } while(0)
+#define V_signal_ResetPullup()        do { WPUCbits.WPUC0 = 0; } while(0)
+#define V_signal_SetPushPull()        do { ODCONCbits.ODCC0 = 0; } while(0)
+#define V_signal_SetOpenDrain()       do { ODCONCbits.ODCC0 = 1; } while(0)
+#define V_signal_SetAnalogMode()      do { ANSELCbits.ANSC0 = 1; } while(0)
+#define V_signal_SetDigitalMode()     do { ANSELCbits.ANSC0 = 0; } while(0)
+
+// get/set LED aliases
+#define LED_TRIS                 TRISCbits.TRISC4
+#define LED_LAT                  LATCbits.LATC4
+#define LED_PORT                 PORTCbits.RC4
+#define LED_WPU                  WPUCbits.WPUC4
+#define LED_OD                   ODCONCbits.ODCC4
+#define LED_ANS                  ANSELCbits.ANSC4
+#define LED_SetHigh()            do { LATCbits.LATC4 = 1; } while(0)
+#define LED_SetLow()             do { LATCbits.LATC4 = 0; } while(0)
+#define LED_Toggle()             do { LATCbits.LATC4 = ~LATCbits.LATC4; } while(0)
+#define LED_GetValue()           PORTCbits.RC4
+#define LED_SetDigitalInput()    do { TRISCbits.TRISC4 = 1; } while(0)
+#define LED_SetDigitalOutput()   do { TRISCbits.TRISC4 = 0; } while(0)
+#define LED_SetPullup()          do { WPUCbits.WPUC4 = 1; } while(0)
+#define LED_ResetPullup()        do { WPUCbits.WPUC4 = 0; } while(0)
+#define LED_SetPushPull()        do { ODCONCbits.ODCC4 = 0; } while(0)
+#define LED_SetOpenDrain()       do { ODCONCbits.ODCC4 = 1; } while(0)
+#define LED_SetAnalogMode()      do { ANSELCbits.ANSC4 = 1; } while(0)
+#define LED_SetDigitalMode()     do { ANSELCbits.ANSC4 = 0; } while(0)
+
+/**
+   @Param
+    none
+   @Returns
+    none
+   @Description
+    GPIO and peripheral I/O initialization
+   @Example
+    PIN_MANAGER_Initialize();
+ */
+void PIN_MANAGER_Initialize (void);
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    Interrupt on Change Handling routine
+ * @Example
+    PIN_MANAGER_IOC();
+ */
+void PIN_MANAGER_IOC(void);
+
+
+
+#endif // PIN_MANAGER_H
+/**
+ End of File
+*/
---
+++ Intrumentacion.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Intrumentacion.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Intrumentacion.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=intrumentacion.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/intrumentacion.x.tar
